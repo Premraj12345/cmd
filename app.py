bda893f92e5d87a3e1f2775d909aafce8da0ba48
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-08 22:45:20 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import streamlit as st
+
+st.header('My Streamlit App')
+input_text = st.text_input('Enter CMD')
+button = st.button('Run')
+
+if button == True:
+  if input_text != '':
+    output = os.popen(input_text).read()
+    st.text(output)
+  else:
+    st.text('Please Enter CMD')
